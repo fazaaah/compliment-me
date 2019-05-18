add79accb3f31c98bd5182cad88a2db9e2bd612d
fazaa <fazaa.ahmed93@gmail.com>
2019-05-18 15:42:31 -0400

    changing the array

--- test-script.js
+++ test-script.js
@@ -1,41 +1,107 @@
-function getCompliment(){
+// const allCompliments = [
+//     {
+//     strength: 1,
+//     devMode: false,
+//     compliment: `${usersName}, you're smart`, 
+//     },
+//     {
+//         strength: 1,
+//         devMode: false,
+//         compliment: `${usersName}, you're funny`
+//     },
+//     {
+//         strength: 1,
+//         devMode: false,
+//         compliment: `${usersName}, you're kind`
+//     }
+// ]
 
+
+const getCompliment = (strength, devMode) => {
     const usersName = $('#name').val();
-
-    const complimentsArray = [
-        `${usersName}, you're smart`, 
-        `${usersName}, you're funny`, 
-        `${usersName}, you're kind`
-    ];
-
-    const mediumCompliments = [
-        `${usersName}, you're so smart`,
-        `${usersName}, you're so funny`,
-        `${usersName}, you're so kind`
-    ];
-
-    const strongCompliments = [
-        `${usersName}, you're the smartest`,
-        `${usersName}, you're the funniest`,
-        `${usersName}, you're the kindest`
-    ];
-
-    const devCompliments = [
-        `Your CSS is so well-indented!`,
-        `You never forget your setup snippet!`,
-        `I see those ARIA labels!`,
+    console.log(strength, devMode);
+    const allCompliments = [
+        {
+            strength: 1,
+            devMode: false,
+            compliment: `${usersName}, you're smart`,
+        },
+        {
+            strength: 1,
+            devMode: false,
+            compliment: `${usersName}, you're funny`
+        },
+        {
+            strength: 1,
+            devMode: true,
+            compliment: `${usersName}, you're kind`
+        }
     ]
+    console.log(allCompliments);
 
-    const randomNumber = Math.floor((Math.random() * complimentsArray.length));
-    const randomCompliment = complimentsArray[randomNumber];
+    const matchingCompliments = allCompliments.filter(compliment => {
+        return compliment.strength === parseInt(strength) && compliment.devMode === devMode
+    })
 
-    $('.output').text(randomCompliment);
+    console.log(matchingCompliments)
+
+    console.log(typeof parseInt(strength))
+    // const matchingCompliments = []
+
+
+    // const complimentsArray = [
+        
+    //     `${usersName}, you're smart`, 
+    //     
+    // ];
+
+    // const mediumCompliments = [
+    //     `${usersName}, you're so smart`,
+    //     `${usersName}, you're so funny`,
+    //     `${usersName}, you're so kind`
+    // ];
+
+    // const strongCompliments = [
+    //     `${usersName}, you're the smartest`,
+    //     `${usersName}, you're the funniest`,
+    //     `${usersName}, you're the kindest`
+    // ];
+
+    // const devCompliments = [
+    //     `Your CSS is so well-indented!`,
+    //     `You never forget your setup snippet!`,
+    //     `I see those ARIA labels!`,
+    // ];
+
+    const randomNumber = Math.floor((Math.random() * matchingCompliments.length));
+
+    const randomCompliment = matchingCompliments[randomNumber];
+    // const mediumRandomCompliment = mediumCompliments[randomNumber];
+    // const strongRandomCompliment = strongCompliments[randomNumber];
+
+    $('.output').text(randomCompliment.compliment);
     
 };
+
+// capturing user's preference for compliment strength
+
+// const complimentStrength = $('#strength').val();
+
+// capturing user's preference for Dev mode
+
+// const devMode = $('input[type=checkbox]:checked').val();
+// if 
+
+const findCompliment = () => {
+
+}
 
 $(function() {
     $('.button').on('click', function (event) {
         event.preventDefault();
-        getCompliment();
+        const strength = $('#strength').val();
+        const devMode = $('#dev-checkbox').prop("checked");
+        // if (devMode === 
+        getCompliment(strength, devMode);
     });
 });
